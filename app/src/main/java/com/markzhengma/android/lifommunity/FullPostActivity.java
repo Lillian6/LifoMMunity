0381faa80ed343e86dc8d522f31c1942536622b5
JieqiongLi <jieqiongli@outlook.com>
2018-04-20 12:07:00 -0400

    add Full page

---
+++ app/src/main/java/com/markzhengma/android/lifommunity/FullPostActivity.java
@@ -0,0 +1,13 @@
+package com.markzhengma.android.lifommunity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_post);
+    }
+}
