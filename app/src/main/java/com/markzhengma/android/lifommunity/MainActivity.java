3236169fab52d07657861904f7e93f38445837aa
JieqiongLi <jieqiongli@outlook.com>
2018-04-24 01:31:35 -0400

    edit recyclerview

--- app/src/main/java/com/markzhengma/android/lifommunity/MainActivity.java
+++ app/src/main/java/com/markzhengma/android/lifommunity/MainActivity.java
@@ -55,6 +55,7 @@ public class MainActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_main, container, false);
+
         initialData();
 //        postIndex = 0;
 
