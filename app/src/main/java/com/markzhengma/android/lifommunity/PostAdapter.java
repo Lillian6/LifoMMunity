b6bdfc3a1c45299520092427728e14331835d6a3
Mark Ma <mazheng1022@hotmail.com>
2018-05-01 08:31:24 -0400

    some minor changes to resolve conflict

--- app/src/main/java/com/markzhengma/android/lifommunity/PostAdapter.java
+++ app/src/main/java/com/markzhengma/android/lifommunity/PostAdapter.java
@@ -103,20 +103,6 @@ public class PostAdapter extends RecyclerView.Adapter<PostViewHolder> {
         holder.getPostTitleView().setText(post.titleText);
     }
 
-        @Override
-        public int getItemCount () {
-            int arr = 0;
-            try {
-                if (posts.size() == 0) {
-                    arr = 0;
-                } else {
-                    arr = posts.size();
-                }
-
-
-            } catch (Exception e) {
-
-            }
 
     @Override
     public int getItemCount() {
@@ -130,11 +116,14 @@ public class PostAdapter extends RecyclerView.Adapter<PostViewHolder> {
 
         } catch (Exception e) {
 
+        }
+
 
             return arr;
         }
 
 
-    }
+
+}
 
 
